19548df7cfa11c9caef10deeaa3e270419dcdf4a
Mohammad Ibraheem <mohammadibrahimsayed@gmail.com>
2026-04-05 15:32:37 +0530

    do hashmap question

---
+++ hashmap/map_hashmap.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{   //input
+  int n;
+  cin>>n;
+  int arr[n];
+  for(int i=0;i<n;i++)
+  {cin>>arr[i];}
+
+    //pre computation
+    map<int,int>mpp;
+    for(int i=0;i<n;i++)
+   { mpp[arr[i]]++;}
+
+
+
+
+  int q;
+  cin>>q;
+  while(q--)
+  {
+    int num;
+    cin>>num;
+    //fetch
+    cout<<mpp[num]<<endl;
+  }
+}
