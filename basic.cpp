bcf0faf167067e60c7c5bf7338d4eaadd58cc775
Mohammad Ibraheem <mohammadibrahimsayed@gmail.com>
2026-06-14 22:49:16 +0530

    revision

--- basic.cpp
+++ basic.cpp
@@ -1,31 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
+vector<bool>vec;
 
-int main()
-{   //input
- string ch;
- cin>>ch;
 
-    //pre computation
-    int maxfrequency=0;
-    map<char,int>mpp;
-    for(int i=0;i<ch.size();i++)
-   { mpp[ch[i]]++;}
-     char a;
-      
-      for(int i=0;i<ch.size();i++)
-      {if(mpp[ch[i]]>maxfrequency)
-       { maxfrequency=mpp[ch[i]];
-        a=ch[i];}
-
-      }
-      
-
-    cout<<"highest frequency is "<<maxfrequency<<" of "<<a;
-
-  
-  
-}
 
 
 
