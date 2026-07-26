d5477c49834c6d1521dec62ac8ce75b26fe26e74
Mohammad Ibraheem <mohammadibrahimsayed@gmail.com>
2026-07-26 23:58:43 +0530

    do cp

--- basic.cpp
+++ basic.cpp
@@ -2,19 +2,5 @@
 using namespace std;
 
 int main(){
- int n;
- cin >> n;
- vector <int> vec;
- 
- for(int i = 1; i <= sqrt(n); i++){
-     if( n % i == 0){
-        vec.push_back(i);
-        if((n/i) != i){
-           vec.push_back(n/i);
-        }
-     }
- }
-  sort(vec.begin(), vec.end());
-  for(auto it : vec)
-  cout << it <<" ";
+
 }
--- binary_exponentiation.cpp
+++ binary_exponentiation.cpp
@@ -6,7 +6,7 @@ int power(int c, int d){
     if(d==0)
         return 1;
 
-    long result = power(c, d/2);
+    long result = power(c, d/2);  
 
     if(d&1){
         return c * result * result;
