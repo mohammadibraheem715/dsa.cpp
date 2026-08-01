fcb1ed869bd87ed97676e7c0fa4ea5c2e5c483bc
Mohammad Ibraheem <mohammadibrahimsayed@gmail.com>
2026-08-01 23:25:51 +0530

    solved prime factoraztion question

---
+++ cp/prime_factor.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> prfac;
+    for( int i = 2; i * i <= n; i++){
+        while(n % i == 0){
+            prfac.push_back(i);
+            n /= i;
+        }
+    }
+    if(n > 1) prfac.push_back(n);
+
+    for(int pr:prfac){
+        cout << pr <<" ";
+    }
+}
