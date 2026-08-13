7dac51c58a3815d2db3d5324a80b29d14459f125
Mohammad Ibraheem <mohammadibrahimsayed@gmail.com>
2026-08-13 23:29:03 +0530

    dsa

--- basic.cpp
+++ basic.cpp
@@ -1,28 +1,13 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int n = 1e7 + 10;
-vector<bool> seive(n,  1);
-vector<int> lw(n,  0),hg(n,  0);
-
-
 int main(){
-    seive[0] = seive[1] = false;
-
-    for(int i = 2; i < n; i++ ){
-        if(seive[i] == true){
-            lw[i] = hg[i] = i;
-            for(int j = 2 * i; j < n; j += i){
-                    seive[j] = false;
-                    hg[j] = i;
-                    if(lw[j] == 0){
-                        lw[j] = i;
-                    }
-            }
-        }
-    }
-
-    for( int i = 1; i < 100; i++){
-        cout <<  lw[i]<< " " << hg[i] << endl;
+    long long int n;
+    cin>>n;
+    n%=4;
+    if (n==1 || n==2){
+        cout<<"1";
+    }else{
+        cout<<"0";
     }
 }
