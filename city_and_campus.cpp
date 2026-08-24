5db75a1f582e0bf823dc8e36ffed3d4b7f53691b
Mohammad Ibraheem <mohammadibrahimsayed@gmail.com>
2026-08-24 15:30:37 +0530

    dsu question

---
+++ city_and_campus.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1e5 + 10;
+
+int parent[N];
+int size[N];
+multiset<int> sizes;
+
+void make(int v){
+    parent[v] = v;
+    size[v] = 1;
+    sizes.insert(1);
+}
+
+int  find(int v){
+    if(parent[v] == v) return v;
+    return parent[v] = find(parent[v]);
+}
+
+void merge(int a, int b){
+    sizes.erase(sizes.find(size[a]));
+    sizes.erase(sizes.find(size[b]));
+
+    sizes.insert(size[a] + size[b]);
+}
+
+void Union(int a, int b){
+    a = find(a);
+    b = find(b);
+
+    if(a != b){
+        if(size[a] < size[b]){
+            swap(a, b);
+        }
+        parent[b] = a;
+        size[a] += size[b];
+    }
+}
+
+int main(){
+   int n, k;
+   cin >>  n >> k;
+    for(int i = 1; i <= n; i++){
+        make(i);
+    }
+
+    while(k--){
+        int u, v;
+        cin >> u >> v;
+        Union(u, v);
+        if(sizes.size() == 1){
+            cout << 0;
+        }
+        else{
+            int max = *(sizes.begin());
+            int min = *(--sizes.end());
+
+            cout << max - min << endl;
+        }
+    }
+
+  
+
+}
