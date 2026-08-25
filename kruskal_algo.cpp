1a89aa39021415aa88da4a243df517191168f2d0
Mohammad Ibraheem <mohammadibrahimsayed@gmail.com>
2026-08-25 23:42:01 +0530

    kruskal algo

---
+++ kruskal_algo.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1e5 + 10;
+
+int parent[N];
+int size[N];
+
+void make(int v){
+    parent[v] = v;
+    size[v] = 1;
+}
+
+int  find(int v){
+    if(parent[v] == v) return v;
+    return parent[v] = find(parent[v]);
+}
+
+void Union(int a, int b){
+    a = find(a);
+    b = find(b);
+
+    if(a != b){
+        if(size[a] < size[b]){
+            swap(a, b);
+        }
+        parent[b] = a;
+        size[a] += size[b];
+    }
+}
+
+int main(){
+    int n, m;
+    cin >> n >> m;
+    vector<pair<int, pair<int, int>>> edges;
+    int u, v, wt;
+    for(int i = 0; i < m; i++){
+        cin >> u >> v >> wt;
+        edges.push_back({wt, {u, v}});
+    }
+    sort(edges.begin(),  edges.end());
+
+    for(int i = 0; i <= n; i++){
+        make(i);
+    }
+
+    int cost = 0;
+    for(auto edge : edges){
+        int wt = edge.first;
+        int u = edge.second.first;
+        int v = edge.second.second;
+        if(find(u) == find(v)) continue;
+        Union(u, v);
+        cost += wt;
+    }
+    cout << cost << endl;
+}
