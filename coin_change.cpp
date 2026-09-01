79f3c2d226325d1738475fe9b98bfa431f54b2c6
Mohammad Ibraheem <mohammadibrahimsayed@gmail.com>
2026-09-01 23:06:09 +0530

    coin problem

---
+++ coin_change.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int N = 1e5 + 10;
+
+int coin(int amt, vector<int>& coins){
+    for(auto coin : coins){
+        
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+
+}
