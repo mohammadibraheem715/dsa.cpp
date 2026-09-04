51195fdd6332d004cbcfce43b3a63a5903343c8a
Mohammad Ibraheem <mohammadibrahimsayed@gmail.com>
2026-09-05 00:12:46 +0530

    corretion

--- reverse_recusion.cpp
+++ reverse_recusion.cpp
@@ -1,15 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n;
 
-int rev(int m){
-    if(m >= (n / 2)) return;
-    swap(m, n-m);
-    rev(m-1);
+
+void rev(int i, string m[], int n){
+    if(i >= n/2){
+        return;
+    }
+    swap(m[i], m[n-i-1]);
+    rev(i+1, m, n);
 }
 
 int main(){
     int n;
     cin >> n;
-    rev(n);
+    string arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    rev(0, arr, n);
+
+    for(int i = 0; i < n; i++){
+        cout << arr[i];
+    }
 }
