2a19be644683d2811114063bcd89c346e9c2147e
Mohammad Ibraheem <mohammadibrahimsayed@gmail.com>
2026-09-09 23:27:08 +0530

    duplicate

--- basic.cpp
+++ basic.cpp
@@ -1,28 +1,19 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        long long ar[n][n];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<=i;j++){
-             if(j==0||j==i){
-                ar[i][j]=1;
-             }
-             else
-             ar[i][j]=ar[i-1][j-1]+ar[i-1][j];          
-              }
-        }
+class solution{
+    bool containsduplicate(vector<int>& nums){
+        int n = nums.size();
+        unordered_map<int,int>v;
 
         for(int i=0;i<n;i++){
-            for(int j=0;j<=i;j++){
-                cout<<ar[i][j]<<" ";
+            v[nums[i]]++;
+            if(v[nums[i]]>1){
+            return true;
             }
-            cout<<endl;
         }
+
+        return false;
+
     }
-}
+};
